d8f540737396a625a3c7661d0d7cefac7385a2fa
Joyce <joycelamsaungan@gmail.com>
2018-02-13 23:08:09 -0800

    modifying function omdb

--- liri.js
+++ liri.js
@@ -2,37 +2,48 @@
 
 
 var request = require("request");
-
 var nodeArg = process.argv;
 
-var movieName = "Mr. Nobody";
+var command = process.argv[2];
 
-for (var i = 2; i < nodeArg.length; i++) {
-	if (i > 2 && i < nodeArg.length) {
-		movieName = movieName + "+" + nodeArg[i];
+function omdb() {
+	var movieName = "";
+
+	if (process.argv[3]) {
+		for (var i = 3; i < nodeArg.length; i++) {
+			if (i > 3 && i < nodeArg.length) {
+				movieName = movieName + "+" + nodeArg[i];
+			} else {
+				movieName += nodeArg[i];
+			}
+		}
 	} else {
-		movieName += nodeArg[i];
+		movieName = "Mr. Nobody";
 	}
+
+	var movieQueryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=520d49c3";
+
+	request(movieQueryUrl, function(error, response, body) {
+		if (!error && response.statusCode === 200) {
+			console.log("\nTitle: " + JSON.parse(body).Title);
+			console.log("\nYear: " + JSON.parse(body).Year);
+			console.log("\nIMDB Rating: " + JSON.parse(body).imdbRating);
+			//console.log("\nRotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
+			console.log("\nCountry: " + JSON.parse(body).Country);
+			console.log("\nLanguage: " + JSON.parse(body).Language);
+			console.log("\nPlot: " + JSON.parse(body).Plot);
+			console.log("\nActors: " + JSON.parse(body).Actors);
+		}
+	})
+
 }
 
-var movieQueryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=520d49c3";
-
-
-request(movieQueryUrl, function(error, response, body) {
-	if (!error && response.statusCode === 200) {
-		console.log("\nTitle: " + JSON.parse(body).Title);
-		console.log("\nYear: " + JSON.parse(body).Year);
-		console.log("\nIMDB Rating: " + JSON.parse(body).imdbRating);
-		console.log("\nRotten Tomatoes Rating: " + JSON.parse(body).Ratings[2].Value);
-		console.log("\nCountry: " + JSON.parse(body).Country);
-		console.log("\nLanguage: " + JSON.parse(body).Language);
-		console.log("\nPlot: " + JSON.parse(body).Plot);
-		console.log("\nActors: " + JSON.parse(body).Actors);
-	}
-})
 
 
 
+if (command === "movie-this") {
+	omdb();
+}
 
 
 
